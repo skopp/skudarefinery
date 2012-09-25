6cea5a9ba47958c5f8d0ab0c9fa210b71249fe98
skopp <skopp888@gmail.com>
2012-09-25 03:13:19 +0200

    'init'

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Skrefine::Application.config.secret_token = 'd718e8db73bed4c387c0cb4cbf5fc69a568ae88499fbabe2e44e739c3f41749bf2e75f962ace2ce3fb1cbcbbe00572c6f3ce038b85787ae1b46a2a7402867822'
